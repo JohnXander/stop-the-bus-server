6ac11f7ff304bc8dcbb45dd6c7ea7da351e470a9
John Xander <johnblox5@gmail.com>
2022-10-14 14:10:39 +0100

    install packages

---
+++ src/index.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT;
+
+const cors = require('cors');
+const morgan = require('morgan');
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('Its working')
+})
+
+app.listen(port, () => {
+    console.log(`\n Server is running on http://localhost:${port}\n`);
+});
